07ea4c592ed1b16dc2079f9b8af82907488823f0
zhangdaihui <zhangdaihui2008@vip.qq.com>
2016-12-09 13:37:24 +0800

    1.移除THINK.get THINK.post THINK.request 2.新增THINK.params THINK.cache/cacheSync 3.用Promise重写THINK.request THINK.getStorage(支持同步、异步) THINK.setStorage(支持同步、异步)

--- pages/index/index.js
+++ pages/index/index.js
@@ -8,36 +8,38 @@ var THINK=app.think();
 Page({
   data: {
     motto: 'Hello World',
-    userInfo: {},
-    array:[
-      {name:'zhangdaihui',age:30},
-      {name:'zhanghanyu',age:2}
-    ]
+    userInfo: {}
   },
   onLoad:function(){
     let text=`
-      <h2 class="title">函数相关</h2>
+      <h2 class="title">新增函数</h2>
       <ul>
-          <li>1.THINK.config<span class="fr">//读取、设置配置文件</span></li>
-          <li>2.THINK.cache<span>//读取、设置本地缓存</span></li>
-          <li>3.THINK.get<span>//读取get参数</span></li>
-          <li>4.THINK.post<span>//读取post参数</span></li>
-          <li>5.THINK.request<span>//读取post参数或get参数，post优先</span></li>
-          <li>6.THINK.data<span>//读取data-参数</span></li>
-          <li>7.THINK.isEmpty<span>//检查是否为空</span></li>
-          <li>8.THINK.html2json<span>//html转成json</span></li>
-          <li>9.THINK.json2html<span>//json转成html</span></li>
-          <li>10.THINK.mixin<span>//非构造函数继承</span></li>
+          <li>THINK.config<span class="fr">//读取、设置配置文件</span></li>
+          <li>THINK.cache/cacheSync<span>//读取、设置本地缓存</span></li>
+          <li>THINK.params<span>//读取post、get、data-参数</span></li>
+          <li>THINK.isEmpty<span>//检查是否为空</span></li>
+          <li>THINK.html2json/json2html<span>//html《=》json</span></li>
+          <li>THINK.mixin<span>//非构造函数继承</span></li>
+          <li>THINK.promise<span>//返回Promise对象</span></li>
+      </ul>
+      <h2 class="title">Promise封装函数</h2>
+      <ul>
+          <li>THINK.request</li>
+          <li>THINK.getStorage<span>//支持同步、异步</span></li>
+          <li>THINK.setStorage<span>//支持同步、异步</span></li>
       </ul>
       <h2 class="title">模板相关</h2>
       <ul>
           <li>
-              <h3>1.支持html格式输出</h3>
+              <h3>支持html格式输出</h3>
           </li>
       </ul>
     `;
     this.setData({
       text:THINK.html2json(text)
+    });
+    THINK.cache('name').then(res=>{
+      console.log(res);
     });
   }
 })
