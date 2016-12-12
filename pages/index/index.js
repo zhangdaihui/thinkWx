b2b53e9b60bab0552f75b3b79e595201ffb4637e
zhangdaihui <zhangdaihui2008@vip.qq.com>
2016-12-12 10:41:45 +0800

    新增部分工具函数

--- pages/index/index.js
+++ pages/index/index.js
@@ -11,16 +11,18 @@ Page({
     userInfo: {}
   },
   onLoad:function(){
+    let aa='123456';
+    console.log(THINK.md5.hex_md5(aa));
     let text=`
       <h2 class="title">新增函数</h2>
       <ul>
           <li>THINK.config<span class="fr">//读取、设置配置文件</span></li>
           <li>THINK.cache/cacheSync<span>//读取、设置本地缓存</span></li>
           <li>THINK.params<span>//读取post、get、data-参数</span></li>
-          <li>THINK.isEmpty<span>//检查是否为空</span></li>
           <li>THINK.html2json/json2html<span>//html《=》json</span></li>
           <li>THINK.mixin<span>//非构造函数继承</span></li>
           <li>THINK.promise<span>//返回Promise对象</span></li>
+          <li>THINK.xxx<span>//新增部分工具函数</span></li>
       </ul>
       <h2 class="title">Promise封装函数</h2>
       <ul>
